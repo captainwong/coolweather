085f4aaadc55d3a50c0698de03f75e8a98dcc6f7
win7_64_en <1281261856@qq.com>
2016-08-14 17:04:04 +0800

    add some packages

--- app/src/main/java/app/coolweather/com/coolweather/activity/MainActivity.java
+++ app/src/main/java/app/coolweather/com/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package app.coolweather.com.coolweather;
+package app.coolweather.com.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import app.coolweather.com.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
